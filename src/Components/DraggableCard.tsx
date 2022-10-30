27d4df08be45bc8272ddf490ab50355fd7c898ab
seonhop <78877741+seonhop@users.noreply.github.com>
2022-10-30 00:56:10 -0400

    modifying styles

--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -2,11 +2,14 @@ import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import React from "react";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
 	border-radius: 5px;
 	padding: 10px;
 	margin-bottom: 10px;
-	background-color: ${(props) => props.theme.cardColor};
+	background-color: ${(props) =>
+		props.isDragging ? "#74b9ff" : props.theme.cardColor};
+	box-shadow: ${(props) =>
+		props.isDragging ? "0px 2px 20px rgba(0, 0, 0, 0.2)" : "none"};
 `;
 
 interface IDraggableCardProps {
@@ -17,8 +20,9 @@ interface IDraggableCardProps {
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
 	return (
 		<Draggable key={toDo} draggableId={toDo} index={index}>
-			{(magic) => (
+			{(magic, snapshot) => (
 				<Card
+					isDragging={snapshot.isDragging}
 					ref={magic.innerRef}
 					{...magic.dragHandleProps}
 					{...magic.draggableProps}
--- src/Components/DroppableBoard.tsx
+++ src/Components/DroppableBoard.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
 
 const Board = styled.div`
-	padding: 20px 10px 20px 10px;
+	padding-top: 10px;
 	background-color: ${(props) => props.theme.boardColor};
 	border-radius: 5px;
 	min-height: 200px;
@@ -25,9 +25,14 @@ interface IAreaProps {
 
 const Area = styled.div<IAreaProps>`
 	background-color: ${(props) =>
-		props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+		props.isDraggingOver
+			? "#dfe6e9"
+			: props.isDraggingFromThis
+			? "#b2bec3"
+			: "transparent"};
 	flex-grow: 1;
 	transition: background-color 0.3s ease-in-out;
+	padding: 20px;
 `;
 
 interface IDroppableBoardProps {
